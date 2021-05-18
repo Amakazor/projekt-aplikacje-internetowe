e116e391f32d4c87b9b8262357e057628b08ed48
amakazor <amakazor@gmail.com>
2021-05-18 14:49:38 +0200

    Update UserFixtures.php

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -1,25 +1,42 @@
 <?php
 namespace App\DataFixtures;
+use App\Entity\Company;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\User;
 
-class UserFixtures extends Fixture
+class UserFixtures extends Fixture implements DependentFixtureInterface
 {
     private $passwordEncoder;
+
     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
     {
         $this->passwordEncoder = $passwordEncoder;
     }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        /** @var Company $company */
+        $company = $this->getReference('company');
+
         $user = new User();
-        $manager->persist($user);
-        //$manager->flush();
+        $user->setUsername('testowy');
         $user->setPassword($this->passwordEncoder->encodePassword($user,'the_new_password'));
+        $user->setCompany($company);
+        $user->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
+        $user->setFirstname('tester');
+        $user->setLastname('testowski');
+
+        $manager->persist($user);
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            CompanyFixtures::class
+        ];
     }
 }
